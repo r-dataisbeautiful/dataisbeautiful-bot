8918a4061554fa2bf449058b449b8a736ca8d0c2
Grayson Martin <grayson@graysonmartin.net>
2020-02-05 15:23:11 -0600

    Finalize app.py

--- app.py
+++ app.py
@@ -9,14 +9,15 @@ reddit = praw.Reddit(client_id=os.environ['CLIENT_ID'],
                      username='dataisbeautiful-bot')
 
 for submission in reddit.subreddit('dataisbeautiful').new(limit=100):
-    if (re.search('[\[\(\{][oO][cC][\]\)\}]',submission.title) is not None) and (submission.link_flair_text is not "OC Approved") and (submission.approved_by is not None):
-        submission.mod.flair(text="OC Approved",css_class="oc")
+    if (re.search('[\[\(\{][oO][cC][\]\)\}]',submission.title) is not None) and (submission.saved is False) and (submission.approved_by is not None):
 
         if submission.author_flair_css_class in ["ocmaker", None, ""]: # ignore other flairs as to avoid overriding the "Practicioner", etc flairs
-            if submission.author_flair_text.startswith("OC: "): # user already has an OC flair
+            if submission.author_flair_text and submission.author_flair_text.startswith("OC: "): # user already has an OC flair
                 oc_count = int(submission.author_flair_text.replace("OC: ","")) # get current flair and format it as an integer
                 oc_count += 1
                 newflair = "OC: {0}".format(oc_count)
                 reddit.subreddit('dataisbeautiful').flair.set(submission.author.name, newflair, "ocmaker") # set new flair
             else: # user has never made an OC before
                 reddit.subreddit('dataisbeautiful').flair.set(submission.author.name, "OC: 1", "ocmaker") # set new flair
+            submission.author.message("r/DataIsBeautiful Submission OC Status Approved","Your post, [{0}]({1}), on r/DataIsBeautiful has been approved for OC status and your OC count has been increased to {2}.".format(submission.title,submission.permalink,oc_count),from_subreddit="dataisbeautiful")
+            submission.save()
